16dca48f5a5589f8bb143d2eb96f90f2300bb9d8
dven-dev <me@dven.dev>
2025-08-05 23:25:30 +0300

    fix ChatPage.jsx

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -97,12 +97,11 @@ const ChatPage = () => {
     dispatch(fetchChannels())
     dispatch(fetchMessages())
   }, [dispatch])
-  
   useEffect(() => {
-  if (currentChannelId) {
-    dispatch(fetchMessages())
-  }
-}, [currentChannelId, dispatch])
+    if (currentChannelId) {
+      dispatch(fetchMessages())
+    }
+  }, [currentChannelId, dispatch])
 
   const handleSend = async (e) => {
     e.preventDefault()
