b90dcfcf67a9e3bf0a799d584fc604a08a7290d9
dven-dev <me@dven.dev>
2025-08-05 17:30:15 +0300

    fix eslint errors

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 const Messages = ({ messages = [] }) => {
   const { t } = useTranslation()
-  
   return (
     <div
       id="messages-box"
